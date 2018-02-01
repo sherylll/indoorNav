b551b0b3132adb6f9a0ae51185c1627512a56158
sherylll <sherylsun14@gmail.com>
2018-02-01 19:12:48 +0100

    latest

--- arduino/step_det/step_det.ino
+++ arduino/step_det/step_det.ino
@@ -1,22 +1,16 @@
+// 90:A2:DA:F8:37:9B
+
 #include <Wire.h>
 #include <SPI.h>
+#include <Bridge.h>
+#include <HttpClient.h>
 #include <Adafruit_LSM9DS1.h>
-#include <Adafruit_Simple_AHRS.h>
 #include <Adafruit_Sensor.h>  // not used in this demo but required!
 #include <math.h>   // for tan2 & fabs
 
 // i2c
 Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
-Adafruit_Simple_AHRS ahrs(&lsm.getAccel(), &lsm.getMag());
-#define LSM9DS1_SCK A5 // SCL
-#define LSM9DS1_MISO 12 // (SDO_M & SDO_AG)
-#define LSM9DS1_MOSI A4 // => SPI MOSI (SDA)
-#define LSM9DS1_XGCS 6 // chip select accelerometer (CSAG)
-#define LSM9DS1_MCS 5 // chip select magnetometer (CSM)
-// You can also use software SPI
-//Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1(LSM9DS1_SCK, LSM9DS1_MISO, LSM9DS1_MOSI, LSM9DS1_XGCS, LSM9DS1_MCS);
-// Or hardware SPI! In this case, only CS pins are passed in
-//Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1(LSM9DS1_XGCS, LSM9DS1_MCS);
+
 
 // vector_t is just the three float vals x, y, z
 #define vector_t sensors_vec_t
@@ -32,37 +26,37 @@ float yaw;
 sensors_vec_t   orientation;
 
 //for moving average
-#define N 50
-int n=0; float yaw_instant[N];
+#define NN 50
+int n=0; float yaw_instant[NN];
 
 //current velocity and distance
 float v = 0, d = 0;
 
 //step detector
-float step_threshold; float last_orient = 0; long last_step_time; long last_turn_time; bool first_stp = true;
-#define STEPSIZE 0.65
+float step_threshold = 400; float turn_threshold = 400; float last_orient = 0; long last_step_time; long last_turn_time; bool first_stp = true;
+#define STEPSIZE 0.6
 
 //position x/y
-float pos[2] = {0,0};
+//should be the same as in html,width=7.7,height=6; right turn=-90
+float pos[2] = {0.1,4};
 float test_pos = 0;
-float orient_est = 180;
+float orient_est = 0;
 #define INITANGLE 30
 
 void Angle_Avg(float ang_x) {
   n++;
-  yaw_instant[n%N] = ang_x;
+  yaw_instant[n%NN] = ang_x;
   
   float sum_aX; 
 
-  if(n>N){
-    for(int i=0; i<N; i++) {
+  if(n>NN){
+    for(int i=0; i<NN; i++) {
       sum_aX += yaw_instant[i];
       
     }
   }
-  yaw = sum_aX / N;
-  
-  //Serial.print("Yaw: "); Serial.print(yaw);   Serial.println(" ° ");
+  yaw = sum_aX / NN;
+  return;
 
 }
 
@@ -71,41 +65,32 @@ void setupSensor()
 {
     // 1.) Set the accelerometer range
     lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G);
-    //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_4G);
-    //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_8G);
-    //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_16G);
+
 
     // 2.) Set the magnetometer sensitivity
     lsm.setupMag(lsm.LSM9DS1_MAGGAIN_4GAUSS);
-    //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_8GAUSS);
-    //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_12GAUSS);
-    //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_16GAUSS);
+
 
     // 3.) Setup the gyroscope
     lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
-    //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_500DPS);
-    //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_2000DPS);
 }
 
 
 void setup() 
 {
-    Serial.begin(115200);
-
-    while (!Serial) {
-        delay(1); // will pause Zero, Leonardo, etc until serial console opens
-    }
-
-    Serial.println("LSM9DS1 data read demo");
+    //Serial.begin(115200);
+    Bridge.begin();
+//    while (!Serial) {
+//        delay(1); // will pause Zero, Leonardo, etc until serial console opens
+//    }
 
     // Try to initialise and warn if we couldn't detect the chip
     if (!lsm.begin())
     {
-        Serial.println("Oops ... unable to initialize the LSM9DS1. Check your wiring!");
         while (1);
     }
-    Serial.println("Found LSM9DS1 9DOF");
-
+    //Serial.println("Found LSM9DS1 9DOF");
+    pinMode(4, INPUT_PULLUP);
     // helper to just set the default scaling we want, see above!
     setupSensor();
     start_time = millis();
@@ -123,51 +108,47 @@ void loop()
     sensors_event_t a, m, g, temp;
 
     lsm.getEvent(&a, &m, &g, &temp);
-
-
     if (current_time - start_time>1000){
+      //Serial.println(g.gyro.z);
       //Step_detector: accel.z value peaks at every step => detect peaks and add one step;  tbd: set the threshold adaptive (roll/yaw correction over an average(?))
       step_threshold = 12;
-      
-      
-//      Serial.print(orientation.heading); Serial.print("   ");
-//      Serial.print(a.acceleration.z); Serial.print("   ");
+      turn_threshold = 130;
+      Angle_Avg(g.gyro.z);
 
-
-        
-  
-  if (millis() - last_turn_time > 1000) {
-  if ((yaw-last_orient > 45) && (yaw + 360 - last_orient > 45)) {
-    Serial.println("Changed + ");
-    Serial.print("last_orient: "); Serial.print(last_orient);   Serial.print(" ° ");
-    Serial.print("yaw: "); Serial.print(yaw);   Serial.print(" ° ");
-    orient_est += 90;
-  }
-  else if ((last_orient - yaw > 45) && (last_orient - (yaw - 360)> 45 )) {
-    Serial.println("Changed - ");
-    Serial.print("last_orient: "); Serial.print(last_orient);   Serial.print(" ° ");
-    Serial.print("yaw: "); Serial.print(yaw);   Serial.print(" ° ");
-    orient_est -= 90;
-  }
-  last_orient = yaw;
-  last_turn_time = millis();
-  }
+      if ( digitalRead(4) == LOW) {
+        pos[0] = 0.1;
+        pos[1] = 4;
+        orient_est = 0;
+        send_orient();
+        delay(100);
+      }
       
-      if (a.acceleration.z > step_threshold && millis() - last_step_time > 300) {
+      if (yaw > turn_threshold && millis() - last_turn_time > 800) {
+
+        last_turn_time = millis();
+        last_step_time = millis();
+        orient_est +=90;
+          //Serial.println("Turn +");
+          send_orient();
+                
+      }
+      if (yaw < -turn_threshold && millis() - last_turn_time > 800) {
+
+        last_turn_time = millis();
+         last_step_time = millis();
+        orient_est -=90;
+          //Serial.println("Turn -");
+          send_orient();
+                
+      }      
+      if (a.acceleration.z > step_threshold && millis() - last_step_time > 500) {
 
         last_step_time = millis();
         execute_step();
-          //Serial.println("1");
-          //Serial.print("Last: "); Serial.print(last_step_time);   Serial.print(" m; ");
-          //Serial.print("Millis: "); Serial.print(millis());   Serial.print(" m; ");
+        //  Serial.println("Step");
                 
       }
 
-        if(abs(a.acceleration.z) < step_threshold && millis()-last_step_time>100){
-          ahrs.getOrientation(&orientation);
-          Angle_Avg(orientation.heading);
-
-        }
     }
 }
 
@@ -176,18 +157,26 @@ void execute_step() {
   test_pos += STEPSIZE;
 
   //last_turn_time = millis();
+
+  pos[0] += cos(orient_est/180*PI) * STEPSIZE;
+  pos[1] += sin(orient_est/180*PI) * STEPSIZE;
   
- 
-  float dummy = orient_est;
+  HttpClient client;
 
-    pos[0] += cos(dummy/180*PI) * STEPSIZE;
-    pos[1] += sin(dummy/180*PI) * STEPSIZE;
-
-  //Serial.print("yaw: "); Serial.print(yaw);  Serial.print("  ");  
-  Serial.print("X: "); Serial.print(pos[0]);   Serial.print(" m; ");
-  Serial.print("Y: "); Serial.print(pos[1]);   Serial.print(" m; ");
-  //Serial.print("last_orient: "); Serial.print(last_orient);   Serial.print(" ° ");
-  //Serial.println(yaw);Serial.print("   ");
-  //Serial.print("Yaw: "); Serial.print(avg_yaw_t);   Serial.print(" °");
-  Serial.println();
+  // Make a HTTP request:
+  //error checking
+  String url = "http://192.168.1.100:8000/map/changePos/" + String(pos[0])+"&"+String(pos[1])+"&"+String(orient_est);
+  client.get(url);
+  return;
 }
+
+void send_orient(){
+  HttpClient client;
+
+  // Make a HTTP request:
+  //error checking
+  String url = "http://192.168.1.100:8000/map/changePos/" + String(pos[0])+"&"+String(pos[1])+"&"+String(orient_est);
+  client.get(url);
+  return;
+  }
+
